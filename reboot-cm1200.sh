d14bb335f5057d7d7f3476ad13bb6b79d1c87a47
superspatula <46855452+superspatula@users.noreply.github.com>
2023-05-28 17:16:05 -0500

    Update reboot-cm1200.sh

--- reboot-cm1200.sh
+++ reboot-cm1200.sh
@@ -2,34 +2,44 @@ retries=0
 statuspage="http://192.168.100.1/RouterStatus.htm"
 formpage="http://192.168.100.1/goform/RouterStatus?id="
 formdata="buttonSelect=2&wantype=dhcp&enable_apmode=0&curlang=English"
-userdata="<your_username>:<your_password>"
+userdata="admin:password"
 
+# basic logging function
+logit(){
+    echo "$(date) - $1" >> ~/reboot-cm1200-log.txt
+}
+
+# retry 6 times, then give up
 until [ $retries -ge 5 ]
 do
-    #requires using a cookie
+    # log into router by requesting status page; requires using a cookie
     html=$(curl --silent --user $userdata $statuspage --anyauth --cookie /dev/null/)
 
+    # verify that login succeeded
     if [[ $html != *"HTTP 401"* ]]
     then
-        #pluck the url from the form action tag; it contains a session id
+        # pluck the url from the form action tag; it contains a session id
         sessionurl=$(xmllint --html --xpath "string(//html/body/form/@action)" 2>/dev/null - <<<"$html")
 
-        #split the url on the "=" character and get the second element which is session id
+        # split the url on the "=" character and get the second element which is session id
         sessionid=$(echo $sessionurl | cut -d"=" -f2)
 
+        # verify that the sessionid is numeric, otherwise, something went wrong
         if [[ $sessionid =~ ^[0-9]+$ ]]
         then
-            echo "sessionid is $sessionid - execute reboot!"
+            logit "sessionid is $sessionid - executing reboot!"
+            
+            # issue reboot command and exit script
             curl --silent --user $userdata --anyauth --cookie /dev/null/ --data $formdata -X POST $formpage$sessionid
             break
         else
-            echo "oops! sessionid is empty or non-numeric: $sessionid"
+            logit "oops! sessionid is empty or non-numeric: $sessionid"
         fi
     else
-        echo "oops! saw HTTP 401."
+        logit "oops! saw HTTP 401."
     fi
 
     ((retries+=1))
-    echo "something went wrong. trying again! retries=$retries"
+    logit "something went wrong. trying again! retries=$retries"
     sleep 1
 done
